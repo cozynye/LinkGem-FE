f2495b001f2165f98f592680ac258f844d3905c7
cozynye <cozynye@gmail.com>
2022-12-07 23:37:38 +0900

    fix:[22.12.17] 젬박스 모달 스타일 수정 중

--- components/gembox/modal/deleteBox.tsx
+++ components/gembox/modal/deleteBox.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from 'recoil';
 import { gemboxRefetch } from 'store/store';
 import { useMutation } from 'utils/useMutation';
 import * as S from '../gembox.styles';
+import { LinkSaveButton } from 'components/Setting/Setting.style';
 
 const DeleteBox = (props: IDeleteBoxProps) => {
   const [deleteBox] = useMutation('delete');
@@ -19,28 +20,59 @@ const DeleteBox = (props: IDeleteBoxProps) => {
 
   return (
     <S.DeleteWrapper>
+      <div className="box">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="16" cy="16" r="16" fill="#FFE0E0" />
+          <path
+            d="M15.52 18.96H17.44L17.86 10.78L17.96 8H15L15.1 10.78L15.52 18.96ZM15.04 23.48H17.96V20.64H15.04V23.48Z"
+            fill="#FF3C3C"
+          />
+        </svg>
+      </div>
       <S.DeleteTitle>정말 잼박스를 삭제할까요?</S.DeleteTitle>
       <div>
         <S.DeleteText>
-          잼박스를 삭제하면 안에 보관된 링크도 함께 사라집니다.
-        </S.DeleteText>
-        <S.DeleteText style={{ fontWeight: 500 }}>
-          삭제된 잼박스, 링크는 복구되지 않습니다.
+          잼박스를 삭제하면 안에 보관된 링크도 함께 사라져요.
         </S.DeleteText>
       </div>
       <S.ButtonWrapper>
-        <S.GemBoxButton
+        <LinkSaveButton
+          color="#616163"
+          bgColor="#FFFFFF"
+          onClick={() => props.setIsDelete(false)}
+          dif={'1'}
+          width="122px"
+          height="42px"
+        >
+          다시 생각할게요
+        </LinkSaveButton>
+        <LinkSaveButton
+          bgColor="#5200FF"
+          color="#FFFFFF"
+          width="122px"
+          height="42px"
+          onClick={onClickDelete}
+        >
+          네, 삭제할게요
+        </LinkSaveButton>
+        {/* <S.GemBoxButton
           onClick={() => props.setIsDelete(false)}
           style={{ padding: '16px 24px', backgroundColor: '#0F0223' }}
         >
-          다시 생각 할게요
+          다시 생각해 볼게요.
         </S.GemBoxButton>
         <S.GemBoxButton
           onClick={onClickDelete}
           style={{ padding: '16px 24px' }}
         >
           네, 삭제할게요
-        </S.GemBoxButton>
+        </S.GemBoxButton> */}
       </S.ButtonWrapper>
     </S.DeleteWrapper>
   );
